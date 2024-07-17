9daabf8d8299810bbe88a9e40c9f63ea1fe414fd
GuillaumeVern <guillaume.vern@outlook.com>
2024-07-17 10:21:39 +0200

    :sparkles: get_user_leaderboard

--- routers/users.py
+++ routers/users.py
@@ -3,6 +3,7 @@ import connector
 import auth
 from typing import Annotated
 from models.user import User
+import json
 
 router = APIRouter()
 db = connector.connect()
@@ -45,7 +46,36 @@ def get_user_me(request: Request, response: Response):
     return None
 
 
-    
+@router.get("/leaderboard", tags=["users"])
+def get_user_leaderboard(request: Request, response: Response):
+    if auth.authenticate_user(token=request.headers["Authorization"]):
+        try:
+            query = "SELECT app_user.id_user, app_user.username, app_user.password, app_user.email, player.id_player, player.pseudo, score.score, score.id_score, COUNT(score.id_score) AS gamesplayed FROM `app_user` JOIN player ON player.id_user = app_user.id_user JOIN score ON player.id_player = score.id_player GROUP BY app_user.id_user ORDER BY score.score DESC; "
+            cursor = db.cursor()
+            cursor.execute(query)
+            results = cursor.fetchall()
+            print(results)
+            cursor.close()
+            usersLength = len(results)
+            userInfo = [{} for i in range(usersLength)]
+            usersLengthRange = range(len(results))
+            print(usersLengthRange)
+            for i in usersLengthRange:
+                print(i)
+                userInfo[i]["username"] = results[i][1]
+                userInfo[i]["email"] = results[i][3]
+                userInfo[i]["password"] = results[i][2]
+                userInfo[i]["pseudo"] = results[i][5]
+                userInfo[i]["highscore"] = results[i][6]
+                userInfo[i]["gamesplayed"] = results[i][8]
+
+            return userInfo
+        except Exception as e:
+            print("get_user_leaderboard: ", e)
+            return None
+    return None
+
+
 
 
 @router.get("/{username}", tags=["users"])
