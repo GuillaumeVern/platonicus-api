e96021edcd149dff6558d2a26577b85283b8008e
Guillaume Vern <97952274+GuillaumeVern@users.noreply.github.com>
2024-07-21 15:40:04 +0200

    Update connector.py

--- connector.py
+++ connector.py
@@ -1,8 +1,8 @@
 import mysql.connector
 
 def connect():
-    # db = mysql.connector.connect(host="losvernos.com", user="root", password="Ynov2024ShadowOfTheErdtree", database="platonicus")
-    db = mysql.connector.connect(host="losvernos.com", user="root", password="", database="platonicus")
+    db = mysql.connector.connect(host="losvernos.com", user="root", password="Ynov2024ShadowOfTheErdtree", database="platonicus")
+    # db = mysql.connector.connect(host="localhost", user="root", password="", database="platonicus")
     db.autocommit = True
     if db.is_connected():
         print("Connected to database")
